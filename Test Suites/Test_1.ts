760cb751d81f73d37aada67f81434dc2718dffc5
Thinkpad X1 Carbon <Thinkpad X1 Carbon@DESKTOP-GRHCEU9>
2023-12-21 14:02:16 +0700

    add test suite

---
+++ Test Suites/Test_1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test_1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4bfd25b0-580f-4283-bfbc-c982b6691f4b</testSuiteGuid>
+   <testCaseLink>
+      <guid>29ff9497-8e8c-4b73-bc74-68715f5340df</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
